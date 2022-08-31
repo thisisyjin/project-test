1d2480b2f065f744884cd095998ac223564e8d1a
poseidon <poseidon-afzima@poseidonui-MacBookPro.local>
2022-08-31 11:35:38 +0900

    public url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Progress from './routes/Progress';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.HOME_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/progress/:step" element={<Progress />} />
